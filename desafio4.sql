54e2cc4bb3ff9cea59edefb86c8dcabadd74fa46
Jean Marcel <jeanvanzella91@gmail.com>
2022-02-15 16:06:05 -0300

    fixing 1 and add 4

---
+++ desafio4.sql
@@ -0,0 +1,6 @@
+SELECT u.user_name AS usuario,
+IF(YEAR(MAX(h.played_date)) = '2021', 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.users AS u
+JOIN SpotifyClone.history AS h ON u.user_id = h.user_id
+GROUP BY usuario
+ORDER BY usuario;
