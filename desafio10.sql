172473dc234e25d93eb880dff143e385fa16e71b
Jean Marcel <jeanvanzella91@gmail.com>
2022-02-22 15:31:40 -0300

    fixing req10

--- desafio10.sql
+++ desafio10.sql
@@ -1,16 +1,12 @@
 SELECT
-  s.song_name AS nome, COUNT(h.song_id) AS reproducoes
+	s.song_name AS nome, COUNT(*) AS reproducoes
 FROM
-  SpotifyClone.songs AS s
+  SpotifyClone.history AS h
 JOIN
-  SpotifyClone.history AS h ON s.song_id = h.song_id
+  SpotifyClone.songs AS s ON s.song_id = h.song_id
 JOIN
-  SpotifyClone.users AS u ON h.user_id = u.user_id
+  SpotifyClone.users AS u ON u.user_id = h.user_id
 WHERE
-  u.plan_id IN (1, 4)
-GROUP BY
-  nome
-ORDER BY
-  nome;
-
---https://github.com/tryber/sd-015-a-mysql-one-for-all/pull/54/files, peguei uma dica de como achar por planos.
+  u.plan_id IN (1,4)
+GROUP BY nome
+ORDER BY nome;
